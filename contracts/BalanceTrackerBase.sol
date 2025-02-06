09d8a3f3418c9fb5df8912f22e838447e9627c86
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-02-06 11:42:58 +0000

    chore: removing comment

--- contracts/BalanceTrackerBase.sol
+++ contracts/BalanceTrackerBase.sol
@@ -259,8 +259,7 @@ abstract contract BalanceTrackerBase {
                     // Return back requester overpayment debit / credit
                     uint256 rateDiff = requesterDeliveryRates[i] - mechDeliveryRates[i];
                     totalRateDiff += rateDiff;
-
-                    // TODO Revisit once NVM is changed
+                    
                     // Adjust requester balance
                     mapRequesterBalances[requesters[i]] = _adjustFinalBalance(requesters[i], rateDiff);
                 }
