982c45c8eff353104e2a1764a3371484fb31b7ee
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-02-20 17:34:18 +0000

    chore: deploying more

--- scripts/deployment/verify_10_balance_tracker_fixed_price_token.js
+++ scripts/deployment/verify_10_balance_tracker_fixed_price_token.js
@@ -4,10 +4,10 @@ const dataFromJSON = fs.readFileSync(globalsFile, "utf8");
 const parsedData = JSON.parse(dataFromJSON);
 const mechMarketplaceProxyAddress = parsedData.mechMarketplaceProxyAddress;
 const drainerAddress = parsedData.drainerAddress;
-const wrappedNativeTokenAddress = parsedData.wrappedNativeTokenAddress;
+const olasAddress = parsedData.olasAddress;
 
 module.exports = [
     mechMarketplaceProxyAddress,
     drainerAddress,
-    wrappedNativeTokenAddress
+    olasAddress
 ];
