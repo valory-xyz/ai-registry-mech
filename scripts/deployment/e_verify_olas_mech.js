b596c1ab10670443e2cc97733bd82656ae3f4bb7
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2025-03-06 10:00:59 +0000

    chore: linters

--- scripts/deployment/e_verify_olas_mech.js
+++ scripts/deployment/e_verify_olas_mech.js
@@ -6,14 +6,14 @@ async function main() {
     const dataFromJSON = fs.readFileSync(globalsFile, "utf8");
     let parsedData = JSON.parse(dataFromJSON);
 
-    const provider = new ethers.providers.JsonRpcProvider(parsedData.networkURL);
-    const signers = await ethers.getSigners();
+    const provider = new hre.ethers.providers.JsonRpcProvider(parsedData.networkURL);
+    const signers = await hre.ethers.getSigners();
 
     const deployer = signers[0];
     console.log("Deployer is:", deployer.address);
 
     const mechAddress = "";
-    const mech = await ethers.getContractAt("MechFixedPriceNative", mechAddress);
+    const mech = await hre.ethers.getContractAt("MechFixedPriceNative", mechAddress);
     const mechMarketplaceAddress = await mech.mechMarketplace();
     const serviceRegistryAddress = await mech.serviceRegistry();
     const serviceId = await mech.tokenId();
